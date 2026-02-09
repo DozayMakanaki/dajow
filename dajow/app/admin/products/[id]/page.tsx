41f6216dd76c8f8bd8008338cff144724d9fc97a
DozayMakanaki <chidoziemail@gmail.com>
2026-02-09 17:34:13 +0100

    last

--- dajow/app/admin/products/[id]/page.tsx
+++ dajow/app/admin/products/[id]/page.tsx
@@ -384,7 +384,7 @@ export default function EditProductPage() {
               ) : (
                 <>
                   <Save className="mr-2 h-4 w-4" />
-                  Save Changes
+                  Save Changes made
                 </>
               )}
             </Button>
