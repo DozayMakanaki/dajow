6abdeb651cf84e93c1ec9eac67fd51ebde871f90
DozayMakanaki <chidoziemail@gmail.com>
2026-02-09 10:48:51 +0100

    lastmm

--- dajow/app/page.tsx
+++ dajow/app/page.tsx
@@ -1,8 +1,8 @@
 import HeroCarousel from "@/components/hero-carousel"
 import CategorySection from "@/components/category-sections"
-import PopularProductsSlider from "@/components/popular-product"
+
 import AboutNewsletter from "@/components/about-newsletter"
-import PopularProducts from "@/components/popular-product"
+
 
 export default function HomePage() {
   return (
