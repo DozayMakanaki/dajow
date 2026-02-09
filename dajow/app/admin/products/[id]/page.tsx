15f98348669be1fe50efeda3d44cef980d1d49d8
DozayMakanaki <chidoziemail@gmail.com>
2026-02-09 17:35:42 +0100

    buullf

--- dajow/app/admin/products/[id]/page.tsx
+++ dajow/app/admin/products/[id]/page.tsx
@@ -379,7 +379,7 @@ export default function EditProductPage() {
               {saving ? (
                 <>
                   <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Saving...
+                  Saving....
                 </>
               ) : (
                 <>
